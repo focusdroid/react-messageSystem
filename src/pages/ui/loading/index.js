0f45d6702f829dd3c07571df8154e97d3379e652
wx <focusdroid@163.com>
2019-01-09 15:55:11 +0800

    loading部分实现

--- src/pages/ui/loading/index.js
+++ src/pages/ui/loading/index.js
@@ -3,11 +3,58 @@ import { Card, Button, Spin, Icon, Alert } from 'antd'
 import '../ul.less'
 
 export default class Index extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      alertState: true
+    }
+  }
   render(){
+    const icon = <Icon type="yahoo"></Icon>
     return (
       <div>
-        loading...
+        <Card
+
+          title="卡片的用法">
+          <Spin
+            spinning={true}
+            size="small"
+          ></Spin>
+          <Spin
+            spinning={true}
+            size="default"
+          ></Spin>
+          <Spin
+            spinning={true}
+            size="large"
+            indicator={icon}
+          ></Spin>
+        </Card>
+        <Card title="内容遮罩">
+         {/* <Alert
+            message="React"
+            description="welcome to beijing"
+            type="info"
+          ></Alert>*/}
+          <Spin
+            spinning={this.state.alertState}
+          >
+            <Alert
+              message="React"
+              description="welcome to alert"
+              type="info"
+            ></Alert>
+          </Spin>
+        </Card>
+        <Card title=""></Card>
       </div>
     )
+  }
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        alertState: false
+      })
+    }, 3000)
   }
 }
