88e1452cd75de2b0701cfcbe9afab4b3f253b731
focusdroid <840254112@qq.com>
2019-01-09 23:16:24 +0800

    新增全局提示update

--- src/pages/ui/message/index.js
+++ src/pages/ui/message/index.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
-
+import { Card, Button, message } from 'antd'
 
 export default class Messages extends Component{
   render(){
     return(
       <div>
-        message
+        <Card title="全局提示">
+          <Button type="primary" onClick={this.message.bind(this, 'success')}>Success</Button>
+          <Button type="primary" onClick={this.message.bind(this, 'error')}>error</Button>
+          <Button type="primary" onClick={this.message.bind(this, 'info')}>info</Button>
+          <Button type="primary" onClick={this.message.bind(this, 'loading')}>loading</Button>
+        </Card>
       </div>
     )
+  }
+  message (type) {
+    if (type === 'success') {
+      message.success('成功', 1);
+    } else if (type === 'loading') {
+      message.loading('请稍后,加载中...', 1);
+    }
   }
 }
