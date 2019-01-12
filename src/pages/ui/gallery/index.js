f98de759d721ffac7157a0c134fa4e471619b7c5
wx <focusdroid@163.com>
2019-01-12 14:39:35 +0800

    画廊功能完成

--- src/pages/ui/gallery/index.js
+++ src/pages/ui/gallery/index.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react'
-import { Card, Row, Col } from 'antd'
+import { Card, Row, Col, Modal } from 'antd'
 
 export default class Gallery extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      imgvisible: false,
+      currentImg: ''
+    }
+  }
   render(){
     const img = [
-      ['1.png', '2.png', '3.png', '4.png']
-      ['5.png', '6.png', '7.png', '7.png']
-      ['8.png', '9.png', '10.png', '11.png']
-      ['12.png', '13.png', '14.png', '15.png']
+      ['1.png', '2.png', '3.png', '4.png', '5.png'],
+      ['6.png', '7.png', '8.png', '10.png', '10.png'],
+      ['11.png', '12.png', '13.png', '14.png', '15.png'],
+      ['16.png', '17.png', '18.png', '19.png', '20.png'],
+      ['21.png', '22.png', '23.png', '24.png', '25.png'],
+      ['26.png', '27.png', '28.png', '29.png', '30.png'],
     ]
     const imgList = img.map((list)=>list.map((item) =>
       <Card
         title="图片列表"
-        cover={<img src={'/gallery/'+item}/>}
+        cover={<img src={'/gallery/'+item} onClick={this.openGeller.bind(this, item)}/>}
       >
         <Card.Meta
           title="图片"
@@ -22,32 +31,28 @@ export default class Gallery extends Component{
     ))
     return(
       <div>
-        <Row>
-          <Col md={5}>
-            {imgList[0]}
-          </Col>
+        <Row gutter={20}>
+          {imgList.map((item, i)=> <Col md={4}>{imgList[i]}</Col>)}
         </Row>
-        <Row>
-          <Col md={5}>
-            {imgList[1]}
-          </Col>
-        </Row>
-        <Row>
-          <Col md={5}>
-            {imgList[2]}
-          </Col>
-        </Row>
-        <Row>
-          <Col md={5}>
-            {imgList[3]}
-          </Col>
-        </Row>
-        <Row>
-          <Col md={5}>
-            {imgList[4]}
-          </Col>
-        </Row>
+        <Modal
+          width={500}
+          height={800}
+          visible={this.state.imgvisible}
+          onCancel={()=>{this.setState({
+            imgvisible: false
+          })}}
+          footer={null}
+        >
+          <img src={this.state.currentImg} alt="" style={{width: '100%', paddingTop: 20}}/>
+        </Modal>
       </div>
     )
+  }
+  openGeller (item) {
+    console.log(item)
+    this.setState({
+      imgvisible: true,
+      currentImg: '/gallery/' + item,
+    })
   }
 }
