4016874cf32b5e444c9c523c499654115e9cca33
wx <focusdroid@163.com>
2019-01-12 16:01:24 +0800

    表单中封装的getFieldDecorator使用

--- src/pages/form/login/index.js
+++ src/pages/form/login/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { Card, Form, Input, Button } from 'antd'
+import { Card, Form, Input, Button, Row, Col } from 'antd'
 import '../../ul.less'
 
 const FormItem = Form.Item
 
 export default class Login extends Component{
   render(){
+    const { getFieldDecorator } = this.props.form; // 检测值的规范
     return (
       <div>
         <Card title="登录行内表单">
@@ -20,6 +21,23 @@ export default class Login extends Component{
               <Button type="primary">登录</Button>
             </FormItem>
           </Form>
+        </Card>
+        <Card title="登录水平表单">
+          <Row>
+            <Col md={7}>
+              <Form layout="horizontal">
+                <FormItem>
+                  <Input placeholder="用户名" size="large"/>
+                </FormItem>
+                <FormItem>
+                  <Input placeholder="密码" size="large"/>
+                </FormItem>
+                <FormItem>
+                  <Button type="primary" size="large">登录</Button>
+                </FormItem>
+              </Form>
+            </Col>
+          </Row>
         </Card>
       </div>
     )
