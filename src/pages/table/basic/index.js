f2ba6c79ce5f701f7b0a68dd2c3e29883636d7a5
wx <focusdroid@163.com>
2019-01-14 10:34:37 +0800

    动态表格动态数据渲染mock数据

--- src/pages/table/basic/index.js
+++ src/pages/table/basic/index.js
@@ -1,40 +1,14 @@
 import React, { Component } from 'react'
 import { Card, Table } from 'antd'
 import '../../ul.less'
-
+import axios from 'axios'
 export default class Basic extends Component{
   constructor(props){
     super(props);
     this.state = {
-      dataSource: []
+      dataSource: [],
+      dataSource2: []
     }
-  }
-  componentDidMount(){
-    const dataSource = [
-      {
-        id: '0',
-        userName: 'focusdroid',
-        sex: '1',
-        state: '1',
-        interest: '1',
-        bith: '2018-01-02',
-        address: '北京市昌平区沙皮头',
-        time: '2019-01-12'
-      },
-      {
-        id: '1',
-        userName: 'focus',
-        sex: '0',
-        state: '0',
-        interest: '1',
-        bith: '2002-01-02',
-        address: '北京市朝阳区沙皮头',
-        time: '2019-01-12'
-      }
-    ]
-    this.setState({
-      dataSource
-    })
   }
   render(){
     const colums = [ // 表头
@@ -81,8 +55,56 @@ export default class Basic extends Component{
             pagination={false}
           />
         </Card>
+        <Card title="动态数据表格">
+          <Table
+            bordered
+            columns={colums}
+            dataSource={this.state.dataSource2}
+            pagination={false}
+          />
+        </Card>
       </div>
     )
+  }
+  requestTableOne () {
+    const dataSource = [
+      {
+        id: '0',
+        userName: 'focusdroid',
+        sex: '1',
+        state: '1',
+        interest: '1',
+        bith: '2018-01-02',
+        address: '北京市昌平区沙皮头',
+        time: '2019-01-12'
+      },
+      {
+        id: '1',
+        userName: 'focus',
+        sex: '0',
+        state: '0',
+        interest: '1',
+        bith: '2002-01-02',
+        address: '北京市朝阳区沙皮头',
+        time: '2019-01-12'
+      }
+    ]
+    this.setState({
+      dataSource
+    })
+  }
+  componentDidMount(){
+    this.requestTableOne()
+    this.requestTableTwo()
+  }
+  requestTableTwo() {
+    let baseUrl = 'https://www.easy-mock.com/mock/5c3bed639f11712a1b2652ac/focusdroid'
+    axios.get(baseUrl+'/table/list').then((res)=>{
+      this.setState({
+        dataSource2: res
+      })
+      console.log(this.state.dataSource2)
+    }).catch()
   }
 }
 
