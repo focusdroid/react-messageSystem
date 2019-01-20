d2661a11476de65533f58c435bcd719285660f24
focusdroid <840254112@qq.com>
2019-01-20 18:29:24 +0800

    富文本清除文本还有一些问题（不能清除）

--- src/pages/rich/index.js
+++ src/pages/rich/index.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react'
 import { Card, Button, Modal } from 'antd'
 import { Editor } from 'react-draft-wysiwyg'
+import draftjs from 'draftjs-to-html'
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 
 export default class Rich extends Component{
   constructor(props){
     super(props);
     this.state = {
+      text: '',
       editorState: '',
-      clearVisible: false
+      clearVisible: false,
+      showTextVisible: false
     }
   }
   render(){
     return (
       <div>
         <Card title="富文本编辑器">
-          <Button style={{marginBottom: 10}} type="primary">显示文本</Button>
+          <Button style={{marginBottom: 10}} type="primary" onClick={this.showText.bind(this)}>显示文本</Button>
           <Button style={{marginBottom: 10}} type="danger" onClick={this.delHandleClick.bind(this)}>清空内容</Button>
           {/*清空内容的确认start*/}
           <div>
@@ -31,9 +34,20 @@ export default class Rich extends Component{
             </Modal>
           </div>
           {/*清空内容的确认end*/}
+          {/*显示文本的modal start*/}
+          <div>
+            <Modal
+              title="文本详情"
+              visible={this.state.showTextVisible}
+              onCancel={this.showTextVisibleCancel}
+            >
+              <h3>{draftjs(this.state.text)}</h3>
+            </Modal>
+          </div>
+          {/*显示文本的modal end*/}
           <Editor
             style={{height: 500}}
-            editorState={this.state.editorState}
+            onContentStateChange={this.ContentStateChange.bind(this)}
             toolbarClassName="toolbarClassName"
             wrapperClassName="wrapperClassName"
             editorClassName="editorClassName"
@@ -45,7 +59,7 @@ export default class Rich extends Component{
   }
   onEditorStateChange (editorState) {
     this.setState({
-      editorState,
+      text: editorState
     });
   }
   delHandleClick () { // 清空信息按钮
@@ -55,7 +69,7 @@ export default class Rich extends Component{
   }
   handleOk = () => { // 确定删除
     this.setState({
-      editorState: '',
+      text: ''
     }, () => {
       this.setState({
         clearVisible: false
@@ -65,6 +79,21 @@ export default class Rich extends Component{
   handleCancel = () => { // 取消
     this.setState({
       clearVisible: false
+    })
+  }
+  showText () {
+    this.setState({
+      showTextVisible: true
+    })
+  }
+  showTextVisibleCancel = ()=>{
+    this.setState({
+      showTextVisible: false
+    })
+  }
+  ContentStateChange = (contentState) => {
+    this.setState({
+      text: contentState
     })
   }
 }
